91c62bb0e25857016ef113d95610055c266df1f3
gem4ik <mgemskij@gmail.com>
2023-09-14 16:41:05 +0300

    work with: pagination users follow button

--- src/features/users/model/usersSlice.ts
+++ src/features/users/model/usersSlice.ts
@@ -2,31 +2,60 @@ import { createSlice } from "@reduxjs/toolkit"
 import { createAppAsyncThunk } from "common/utils/create-app-async-thunk"
 import { Params, usersAPI } from "features/users/api/userAPI"
 import { User, UtilResponse } from "common/types/apiTypes"
+import { followAPI } from "features/users/api/followAPI"
+import { ResultCode } from "common/enums/enums"
 
 const getUsers = createAppAsyncThunk<UtilResponse<User[]>, Params>("users/getUsers", async (arg, thunkAPI) => {
-  const { rejectWithValue } = thunkAPI
   const res = await usersAPI.users(arg)
   return res.data
+})
+const followUser = createAppAsyncThunk<{ userId: number }, number>("users/followUser", async (userId, thunkAPI) => {
+  const { rejectWithValue } = thunkAPI
+  const res = await followAPI.follow(userId)
+  if (res.data.resultCode === ResultCode.Success) {
+    return { userId }
+  } else {
+    return rejectWithValue({ data: res.data })
+  }
+})
+const unfollowUser = createAppAsyncThunk<{ userId: number }, number>("users/unfollowUser", async (userId, thunkAPI) => {
+  const { rejectWithValue } = thunkAPI
+  const res = await followAPI.unFollow(userId)
+  if (res.data.resultCode === ResultCode.Success) {
+    return { userId }
+  } else {
+    return rejectWithValue({ data: res.data })
+  }
 })
 
 const slice = createSlice({
   name: "users",
   initialState: {
     users: [] as User[],
-    pageSize: 10,
-    currentPage: 1,
-    totalUsersCount: 0,
-    followed: false,
+    totalUsersCount: 30000,
     isLoading: false
   },
   reducers: {},
   extraReducers: (builder) => {
-      builder
-        .addCase(getUsers.fulfilled, (state, action)=>{
-           state.users = action.payload.items
-        })
+    builder
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.users = action.payload.items
+        state.totalUsersCount = action.payload.totalCount
+      })
+      .addCase(followUser.fulfilled, (state, action) => {
+        const user = state.users.find((u) => u.id === action.payload.userId)
+        if (user) {
+          user.followed = true
+        }
+      })
+      .addCase(unfollowUser.fulfilled, (state, action) => {
+        const user = state.users.find((u) => u.id === action.payload.userId)
+        if (user) {
+          user.followed = false
+        }
+      })
   }
 })
 
 export const usersSlice = slice.reducer
-export const usersThunk = { getUsers }
+export const usersThunk = { getUsers, followUser, unfollowUser }
--- src/features/users/ui/Users.tsx
+++ src/features/users/ui/Users.tsx
@@ -1,28 +1,83 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useActions } from "common/hooks/useActions"
 import { usersThunk } from "features/users/model/usersSlice"
 import { useSelector } from "react-redux"
 import { AppRootState } from "app/store"
+import { User } from "common/types/apiTypes"
+import s from "./Users.module.css"
+import Card from "antd/lib/card/Card"
+import Meta from "antd/lib/card/Meta"
+import Avatar from "antd/lib/avatar/avatar"
+import { Button } from "antd/lib/radio"
+import { Pagination } from "antd"
+
 
 export const Users = () => {
 
-  const { getUsers } = useActions(usersThunk)
+  const { getUsers, followUser, unfollowUser } = useActions(usersThunk)
 
-  const pageSize = useSelector<AppRootState, number>(state => state.users.pageSize)
-  const currentPage = useSelector<AppRootState, number>(state => state.users.currentPage)
+  const users = useSelector<AppRootState, User[]>(state => state.users.users)
+  const totalUsersCount = useSelector<AppRootState, number>(state => state.users.totalUsersCount)
 
-  const params = {
-    count: pageSize,
-    page: currentPage
-  }
+
+  const [params, setParams] = useState({
+    count: 10,
+    page: 1
+  })
 
   useEffect(() => {
     getUsers(params)
-  }, [])
+  }, [params])
+
+  const subscribeHandler = (userId: number, isFollowed: boolean) => {
+    if (!isFollowed) {
+      followUser(userId)
+    } else {
+      unfollowUser(userId)
+    }
+  }
+  const onShowSizeChange = (current: number, pageSize: number) => {
+    setParams({...params, page: current, count: pageSize});
+  }
+
+  const onPageChange = (page: number, pageSize: number) => {
+    setParams({...params, page, count: pageSize});
+  }
 
   return (
     <div>
-      xcvbc
+      <Pagination
+        onChange={onPageChange}
+        showSizeChanger
+        onShowSizeChange={onShowSizeChange}
+        defaultCurrent={params.page}
+        total={totalUsersCount} />
+      <div className={s.usersWrapper}>
+        {users.map(u => {
+          return (
+            <Card
+              style={{ width: 300, height: "max-content", backgroundColor: "#148ec0" }}
+              actions={[
+                <Button
+                  key="subscription"
+                  onClick={() => subscribeHandler(u.id, u.followed)}
+                  type="primary">
+                  {!u.followed ? "Follow" : "Unfollow"}
+                </Button>
+              ]}
+            >
+              <Meta avatar={u.photos.large || u.photos.small ? <img
+                  className={s.avatar}
+                  src={`${u.photos.large ? u.photos.large : u.photos.small}`}
+                  alt="avatar" /> :
+                <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
+                    title={u.name}
+                    description={u.status}
+              />
+            </Card>
+          )
+        })}
+      </div>
     </div>
   )
 }
